594eed2ef80dd317cafa0fe303442bd0b08f51bb
Ian-Grant <iagrant14@gmail.com>
2017-12-14 22:53:06 -0800

    Halfway through video

--- baby1/video.hs
+++ baby1/video.hs
@@ -1,4 +1,13 @@
 import Data.List
 import System.IO
 
-sumOfNums = sum [1..1000]
+addThree x y z = x + y + z
+
+whatGrade :: Int -> String
+whatGrade age
+    | (age >= 5) && (age <= 6) = "Kindergarten"
+    | (age > 6) && (age <= 10) = "Elementary School"
+    | (age > 10) && (age <= 14) = "Middle School"
+    | (age > 14) && (age <= 18) = "High School"
+    | otherwise = "Go to college"
+
