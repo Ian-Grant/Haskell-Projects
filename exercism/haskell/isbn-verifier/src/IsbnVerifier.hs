bead4eda2731ae1407bb6af0395cca7ee254b089
Ian-Grant <iagrant14@gmail.com>
2017-12-17 11:05:05 -0800

    ISBN

--- exercism/haskell/isbn-verifier/src/IsbnVerifier.hs
+++ exercism/haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -3,25 +3,38 @@ import Data.Char
 import Data.List
 ------------2nd Iteration--------------------------
 isbn :: String -> Bool
-isbn x
-    | ((x !! 2 /= '-')&&(last x == 'X')) = isbnHelper2' x == 0
-    | ((x !! 2 /= '-')&&(last x /= 'X')) = isbnHelper2 x == 0
-    | ((x !! 2 == '-')&&(last x == 'X')) = isbnHelper2D' x == 0
-    | ((x !! 2 == '-')&&(last x /= 'X')) = isbnHelper2D x == 0
+isbn x = isbnHelper2 0 x == 0
 
 isbnHelper2 :: Int -> String -> Int
-isbnHelper2 a [] = 0
-isbnHelper2 0 x = ord(x!!0) + isbnHelper2 1 x
-isbnHelper2 1 x = ord(x!!1) + isbnHelper2 2 x
-isbnHelper2 2 x = ord(x!!2) + isbnHelper2 3 x
-isbnHelper2 3 x = ord(x!!3) + isbnHelper2 4 x
-isbnHelper2 4 x = ord(x!!4) + isbnHelper2 5 x
-isbnHelper2 5 x = ord(x!!5) + isbnHelper2 6 x
-isbnHelper2 6 x = ord(x!!6) + isbnHelper2 7 x
-isbnHelper2 7 x = ord(x!!7) + isbnHelper2 8 x
-isbnHelper2 8 x = ord(x!!8) + isbnHelper2 9 x
-isbnHelper2 9 x = ord(x!!9) + isbnHelper2 10 x
-
+isbnHelper2 y [] = 0
+isbnHelper2 0 x = (ord(x!!0)*10) + isbnHelper2 1 x
+isbnHelper2 1 x
+    | (x !! 2 /= '-') = (ord(x!!1)*9) + isbnHelper2 2 x
+    | (x !! 2 == '-') = (ord(x!!2)*9) + isbnHelper2 2 x
+isbnHelper2 2 x
+    | (x !! 2 /= '-') = ord(x!!2)*8) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!3)*8) + isbnHelper2 3 x
+isbnHelper2 3 x
+    | (x !! 2 /= '-') = ord(x!!3)*7) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!4)*8) + isbnHelper2 3 x
+isbnHelper2 4 x = (ord(x!!4)*6) + isbnHelper2 5 x
+    | (x !! 2 /= '-') = ord(x!!4)*6) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!6)*8) + isbnHelper2 3 x
+isbnHelper2 5 x = (ord(x!!5)*5) + isbnHelper2 6 x
+    | (x !! 2 /= '-') = ord(x!!5)*5) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!7)*8) + isbnHelper2 3 x
+isbnHelper2 6 x = (ord(x!!6)*4) + isbnHelper2 7 x
+    | (x !! 2 /= '-') = ord(x!!6)*4) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!8)*8) + isbnHelper2 3 x
+isbnHelper2 7 x = (ord(x!!7)*3) + isbnHelper2 8 x
+    | (x !! 2 /= '-') = ord(x!!7)*3) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!9)*8) + isbnHelper2 3 x
+isbnHelper2 8 x = (ord(x!!8)*2) + isbnHelper2 9 x
+    | (x !! 2 /= '-') = ord(x!!8)*2) + isbnHelper2 3 x
+    | (x !! 2 == '-') = ord(x!!10)*8) + isbnHelper2 3 x
+isbnHelper2 9 x = (ord(x!!9)*1) + isbnHelper2 10 x
+    | (x !! 2 /= '-') = ord(x!!9)*1)
+    | (x !! 2 == '-') = ord(x!!12)*1)
 -----------1st Iteration----------------------
 isbnx :: String -> Bool
 isbnx x
