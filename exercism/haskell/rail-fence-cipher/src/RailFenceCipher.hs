b496db2bafcd4c727c252b335de3e2586ad71461
Ian-Grant <iagrant14@gmail.com>
2017-12-21 23:04:00 -0800

    LYAH 1-5

--- exercism/haskell/rail-fence-cipher/src/RailFenceCipher.hs
+++ exercism/haskell/rail-fence-cipher/src/RailFenceCipher.hs
@@ -1,7 +1,11 @@
 module RailFenceCipher (encode, decode) where
 
 encode :: Int -> String -> String
-encode = error "You need to implement this function!"
+encode rails mess
+        | rails > ((length mess) -1) = error "You can't have more Rails than the length of the message -1"
+        |
+
+
 
 decode :: Int -> String -> String
 decode = error "You need to implement this function!"
