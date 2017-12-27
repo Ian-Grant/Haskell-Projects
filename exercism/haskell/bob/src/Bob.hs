4d6c047799e2301770377d7e84e3e438d13c2e9f
Ian-Grant <iagrant14@gmail.com>
2017-12-27 11:47:58 -0800

    Finished Bob. Added Pangram

--- exercism/haskell/bob/src/Bob.hs
+++ exercism/haskell/bob/src/Bob.hs
@@ -1,4 +1,13 @@
 module Bob (responseFor) where
+import Data.Char
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+    | all isSpace xs = "Fine. Be that way!"
+    | last xs == '?' && yell xs = "Calm down, I know what I'm doing!"
+    | yell xs = "Whoa, chill out!"
+    | last (filter (not . isSpace) xs) == '?' = "Sure."
+    | otherwise = "Whatever."
+
+yell :: String -> Bool
+yell xs = any isAlpha xs && all isUpper (filter isLetter xs)
