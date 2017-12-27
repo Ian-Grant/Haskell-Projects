9ebda6f179412d44d320ef35222ca6a85340c79c
Ian-Grant <iagrant14@gmail.com>
2017-12-26 23:19:30 -0800

    Finished Space Age

--- exercism/haskell/space-age/src/SpaceAge.hs
+++ exercism/haskell/space-age/src/SpaceAge.hs
@@ -8,6 +8,22 @@ data Planet = Mercury
             | Saturn
             | Uranus
             | Neptune
+    deriving Eq
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = (fromInteger $ round $ age * (10^2)) / (10.0^^2)
+    where age
+            | planet == Earth = secToYears seconds
+            | planet == Mercury = secToYears seconds / 0.2408467
+            | planet == Venus = secToYears seconds / 0.61519726
+            | planet == Mars = secToYears seconds / 1.8808158
+            | planet == Jupiter = secToYears seconds / 11.862615
+            | planet == Saturn = secToYears seconds / 29.447498
+            | planet == Uranus = secToYears seconds / 84.016846
+            | planet == Neptune = secToYears seconds / 164.79132
+
+secToYears :: Float -> Float
+secToYears x = x / 31557600.0
+
+round2 :: Float -> Float
+round2 x = (fromInteger $ round $ x * (10^2)) / (10.0^^2)
