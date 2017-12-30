bcd33d4210374f3e53160e29798ad4165d131f89
Ian-Grant <iagrant14@gmail.com>
2017-12-29 22:16:58 -0800

    RNA + ADDED SUM

--- exercism/haskell/rna-transcription/src/DNA.hs
+++ exercism/haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,18 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Maybe String
-toRNA xs = error "You need to implement this function."
+toRNA xs = if checkRNA xs then Just $ map rna xs else Nothing
+    where
+        rna x
+            | x == 'A' = 'U'
+            | x == 'T' = 'A'
+            | x == 'C' = 'G'
+            | x == 'G' = 'C'
+checkRNA :: String -> Bool
+checkRNA rnaS = check rnaS
+    where
+        check :: String -> Bool
+        check [] = True
+        check (x:xs)
+            | x == 'A' || x == 'T' || x == 'C' || x == 'G' = check xs
+            | otherwise = False
