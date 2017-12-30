5275fc7e6377188f1b6f449d72d99c3f9f9c2219
Ian-Grant <iagrant14@gmail.com>
2017-12-29 16:22:02 -0800

    finished collatz

--- exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,10 @@
 module CollatzConjecture (collatz) where
 
+collatz' :: Integer -> Integer -> Maybe Integer
+collatz' x y
+    | y == 1 = Just x
+    | even y = collatz' (x+1) (y `div` 2)
+    | otherwise = collatz' (x+1) ((3*y)+1)
+
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz y = if y <= 0 then Nothing else collatz' 0 y
