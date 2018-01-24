662aaf4be6faa926ec7bc72e08975c86756f6c92
Ian-Grant <iagrant14@gmail.com>
2018-01-24 15:55:15 -0800

    CH15

---
+++ BookExercises/FirstPrinciples/ch15/Optional.hs
@@ -0,0 +1,12 @@
+module Optional where
+
+import Data.Monoid
+
+data Optional a = Nada | Only a deriving (Eq,Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend (Only x) (Only y) = Only $ mappend x y
+  mappend (Only x) _ = Only x
+  mappend _ (Only y) = Only y
+  mappend _ _ = Nada
