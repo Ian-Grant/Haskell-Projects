ca6883c53de745bde8e338716262f13ec62f583a
Ian-Grant <iagrant14@gmail.com>
2018-01-28 23:15:07 -0800

    Grains

--- exercism/haskell/grains/src/Grains.hs
+++ exercism/haskell/grains/src/Grains.hs
@@ -1,7 +1,12 @@
 module Grains (square, total) where
 
+import Data.Maybe
+
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n
+    | n <= 0 = Nothing
+    | n > 64 = Nothing
+    | otherwise = Just $ 2^ pred n
 
 total :: Integer
-total = error "You need to implement this function."
+total = sum $ concatMap (maybeToList . square) [1..64]
