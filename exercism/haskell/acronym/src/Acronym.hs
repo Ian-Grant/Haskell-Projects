1282dd4f55c516be97928560dbf3b2679cd2689c
Ian-Grant <iagrant14@gmail.com>
2018-01-29 11:32:02 -0800

    Hamming

--- exercism/haskell/acronym/src/Acronym.hs
+++ exercism/haskell/acronym/src/Acronym.hs
@@ -1,6 +1,14 @@
 module Acronym (abbreviate) where
 
-import Data.Char (toUpper)
+import Data.Char (toUpper,isUpper,isAlpha)
 
 abbreviate :: String -> String
-abbreviate xs = map (toUpper . head) $ words xs
+abbreviate = concatMap treiLetter . words . map repNonAlpha
+
+repNonAlpha :: Char -> Char
+repNonAlpha x = if isAlpha x then x else ' '
+
+treiLetter :: String -> String
+treiLetter [] = []
+treiLetter (x:xs) = toUpper x : rest
+  where rest = if all isUpper xs then [] else filter isUpper xs
