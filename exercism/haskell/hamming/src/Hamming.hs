053d06e1344a75cf88e73e83cc36101b2103b950
Ian-Grant <iagrant14@gmail.com>
2018-02-03 11:02:00 -0800

    Hamming Finished

--- exercism/haskell/hamming/src/Hamming.hs
+++ exercism/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,17 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "You need to implement this function."
+distance "" "" = Just 0
+distance _ "" = Nothing
+distance "" _ = Nothing
+distance xs ys
+            | xs == ys = Just 0
+            | length xs /= length ys = Nothing
+            | otherwise = Just $ countDif $ zipWith cmp xs ys
+
+cmp :: Char -> Char -> Bool
+cmp x y = x==y
+
+countDif :: [Bool] -> Int
+countDif [] = 0
+countDif (x:xs) = if x then 0+countDif xs else 1 + countDif xs
