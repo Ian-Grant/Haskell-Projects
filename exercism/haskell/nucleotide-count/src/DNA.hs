ed0d76eb8020c10c28766d726e6658108bff8d7a
Ian-Grant <iagrant14@gmail.com>
2018-02-03 16:04:11 -0800

    Nucleotides

--- exercism/haskell/nucleotide-count/src/DNA.hs
+++ exercism/haskell/nucleotide-count/src/DNA.hs
@@ -1,6 +1,19 @@
 module DNA (nucleotideCounts) where
 
 import Data.Map (Map)
+import qualified Data.Map as Map
+
+valLet = "ACGT"
+
+emptyCount :: Map Char Int
+emptyCount = Map.fromList [('A', 0) , ('C', 0) , ('G', 0) , ('T', 0)]
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts = foldr countLet $ Right emptyCount
+
+countLet :: Char -> Either String (Map Char Int) -> Either String (Map Char Int)
+countLet _ (Left s) = Left s
+countLet ch (Right nucMap)
+                | check == True = Right $ Map.insertWith (+) ch 1 nucMap
+                | otherwise = Left $ "ERROR: Wrong Char: " ++ [ch]
+                where check = elem ch valLet
