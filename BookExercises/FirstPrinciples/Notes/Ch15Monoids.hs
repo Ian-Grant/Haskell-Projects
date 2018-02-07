6187ff805020a6b20b5a31583ea634c9e4d3d23b
Ian-Grant <iagrant14@gmail.com>
2018-02-06 21:30:32 -0800

    NNOTES

--- BookExercises/FirstPrinciples/Notes/Ch15Monoids.hs
+++ BookExercises/FirstPrinciples/Notes/Ch15Monoids.hs
@@ -23,5 +23,26 @@ Algebra - Some operation and the set they operate over
 -- How Monoids are defined in Haskell
 --     Typeclasses are a way to recognize, organize, and use common functionality
 --                                                   and patterns across types.
+--     Instances are the operators that operate over the Typeclasses which are
+--     the sets
+--
+-- Mappend is how two values in the type are joined together
+--      Like sum
+-- Mconcat is how two values in the type are joined together
+--      Like mul
+-- Mempty is the identity for mappend
+--
+-- Why Integer lacks a monoidic instance
+--      REPL can't figureout if it is supossed to have a sum or product to join
+--           items together
+--      Sum and Product newtypes are used to tell which should be ussed as
+--              mappend operation
+--      Integrals for a monoid under summations and multiplication
+--
+-- NewType
+--      Newtype constrains the datatype to having a single unary data
+--          constructor aka single data constructor with 1 input
+--      Newtype guarantees no additional runtime overhead in wrapping the
+--          original type. No additional boxing is needed for products and sums
 --
 --
