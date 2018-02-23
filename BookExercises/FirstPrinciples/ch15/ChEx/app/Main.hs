9f956bad0136d5499daf63f5e25f0ca2e7d2e078
Ian-Grant <iagrant14@gmail.com>
2018-02-23 13:15:11 -0800

    CH15

--- BookExercises/FirstPrinciples/ch15/ChEx/app/Main.hs
+++ BookExercises/FirstPrinciples/ch15/ChEx/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import ChEx
 
 main :: IO ()
 main = someFunc
--- BookExercises/FirstPrinciples/ch15/ChEx/src/ChEx.hs
+++ BookExercises/FirstPrinciples/ch15/ChEx/src/ChEx.hs
@@ -1,16 +1,8 @@
 module ChEx where
 
 import Data.Monoid
-import Data.Semigroup
 import Test.QuickCheck
 
-data Trivial = Trivial deriving (Eq, Show)
-
-instance Semigroup Trivial where
-    arbitrary = return Trivial
-
-semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
-semigroupAssoc a b c = (a <> (b <> C)) == ((a <> b) <> c)
-
 monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
-monoidAssoc a b c = (a <> (b <> C)) == ((a <> b) <> c)
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
