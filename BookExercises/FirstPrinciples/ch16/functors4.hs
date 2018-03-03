45efb6e03684f086c40e6c4e6d31c392d2b00c1f
Ian-Grant <iagrant14@gmail.com>
2018-03-02 18:13:43 -0800

    CH16

---
+++ BookExercises/FirstPrinciples/ch16/functors4.hs
@@ -0,0 +1,8 @@
+module Functor4 where
+
+data WhoCares a = ItDoesnt | Matter a | WhatThisIsCalled deriving (Eq, Show)
+
+instance Functor WhoCares where
+    fmap _ ItDoesnt = ItDoesnt
+    fmap _ WhatThisIsCalled = WhatThisIsCalled
+    fmap f (Matter a) = Matter (f a)
